62dde60457632f36b9a4dafe5b720a171f2c38a1
tettekete <6535634+tettekete@users.noreply.github.com>
2025-01-29 01:11:28 +0900

    chore: add configuration definitions and corresponding loader class

---
+++ src/lib/vsc-config.ts
@@ -0,0 +1,96 @@
+
+import * as vscode from 'vscode';
+
+export class VSCConfig
+{
+	static #config = vscode.workspace.getConfiguration();
+
+	// - - - - - - - - - - - - - - - - - - - -
+	// indentation<string>
+	// - - - - - - - - - - - - - - - - - - - -
+	static indentation( fallback?: string ):string | undefined
+	{
+		return VSCConfig._stringConfig(
+			'functionList.indentation'
+			,fallback
+		);
+	}
+
+
+	// - - - - - - - - - - - - - - - - - - - -
+	// indentString<string>
+	// - - - - - - - - - - - - - - - - - - - -
+	static indentString( fallback?: string ):string | undefined
+	{
+		return VSCConfig._stringConfig(
+			'functionList.indentString'
+			,fallback
+		);
+	}
+
+	// - - - - - - - - - - - - - - - - - - - -
+	// prefixString<string>
+	// - - - - - - - - - - - - - - - - - - - -
+	static prefixString( fallback?: string ):string | undefined
+	{
+		return VSCConfig._stringConfig(
+			'functionList.prefixString'
+			,fallback
+		);
+	}
+
+
+	// - - - - - - - - - - - - - - - - - - - -
+	// showSymbolKind<boolean>
+	// - - - - - - - - - - - - - - - - - - - -
+	static showSymbolKind( fallback?: boolean ):boolean | undefined
+	{
+		return VSCConfig._booleanConfig(
+			'functionList.showSymbolKind'
+			,fallback
+		);
+	}
+
+	// - - - - - - - - - - - - - - - - - - - -
+	// private utilities
+	// - - - - - - - - - - - - - - - - - - - -
+	static _stringConfig( configName: string , fallback?: string ):string | undefined
+	{
+		const value = vscode.workspace
+			.getConfiguration()
+			.get<string>( configName );
+		
+		if( value === undefined && typeof fallback === 'string' )
+		{
+			return fallback;
+		}
+
+		return value;
+	}
+
+	static _numberConfig( configName: string , fallback?:number ):number | undefined
+	{
+		const value = vscode.workspace
+			.getConfiguration()
+			.get<number>( configName );
+		
+		if( value === undefined && typeof fallback === 'number' )
+		{
+			return fallback;
+		}
+		return value;
+	}
+
+	static _booleanConfig( configName: string , fallback?:boolean ):boolean | undefined
+	{
+		const value = vscode.workspace
+			.getConfiguration()
+			.get<boolean>( configName );
+		
+		if( value  === undefined && typeof fallback === 'boolean' )
+		{
+			return fallback;
+		}
+		return value;
+	}
+}
