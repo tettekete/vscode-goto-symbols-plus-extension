8a5eb5fc20fd92f9b961222afe4df528cd3e0db0
tettekete <6535634+tettekete@users.noreply.github.com>
2025-02-05 07:20:11 +0900

    build: add parsing module for Makefile descriptions
    - Implemented a module to parse and extract description strings from Makefile
      entries.
    - Enables better context display in QuickPick and other UI components.

---
+++ src/lib/makefile-force-description-extractor.ts
@@ -0,0 +1,39 @@
+import * as vscode from 'vscode';
+
+import type {
+	ExQuickPickItem,
+	FlattenSymbolRec
+} from '../types';
+
+import { VSCContext } from './vsc-context';
+
+export function makefileForceDescriptionExtractor( symbolRec:FlattenSymbolRec ,qpItem:ExQuickPickItem ):ExQuickPickItem
+{
+	const editor = VSCContext.editor();
+
+	const rangeText = editor.document.getText( symbolRec.symbol.range );
+	const dependenceMatch = rangeText.match(/^[^:]+:\s*(.+)/);
+
+	if( ! dependenceMatch )
+	{
+		return qpItem;
+	}
+
+	let dependence = dependenceMatch[1];
+	let lineNo = symbolRec.symbol.range.start.line;
+	while( /\\$/.test( dependence ) )
+	{
+		dependence = dependence.replace( /\\$/,'' );
+		const nextTextLine = editor.document.lineAt( ++ lineNo );
+		let nextLine = nextTextLine.text.trim();
+
+		dependence += ` ${nextLine}`;
+	}
+
+	if( dependence.length )
+	{
+		qpItem['description'] = dependence;
+	}
+
+	return qpItem;
+}
