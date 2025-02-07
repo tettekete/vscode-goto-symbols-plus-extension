b84b0877a2a066666b5cdc7687ac83eec4e0b063
tettekete <6535634+tettekete@users.noreply.github.com>
2025-02-07 10:28:08 +0900

    feat: add explicit support for Go
    - Included Go as an explicitly supported language.
    - Verified that `package` is not listed as a symbol, allowing standard function/class
      processing.

--- src/show-restricted-symbols.ts
+++ src/show-restricted-symbols.ts
@@ -173,6 +173,7 @@ export async function showRestrictedSymbols()
 		case 'java':
 		case 'python':
 		case 'c':
+		case 'go':
 		case 'csharp':
 			quickPickItems = functionsOrClassesList({ documentSymbols });
 			break;
